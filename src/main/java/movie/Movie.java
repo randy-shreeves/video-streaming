259ae5a6fa5c1b9e7041784f7509fef0923e8916
randy <shreevesrandy@gmail.com>
2026-06-24 15:47:57 -0400

    Create movie/ package and implement Movie entity.

---
+++ src/main/java/movie/Movie.java
@@ -0,0 +1,56 @@
+package movie;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false)
+    private int releaseYear;
+    
+    private int runtimeMinutes;
+
+    @Column(nullable = false)
+    private String storageLocation;
+
+    public Movie(){}
+
+    public Movie(String title, String description, int releaseYear, int runtimeMinutes, String storageLocation) {
+        this.title = title;
+        this.description = description;
+        this.releaseYear = releaseYear;
+        this.runtimeMinutes = runtimeMinutes;
+        this.storageLocation = storageLocation;
+    }
+
+    public Long getId() {return id;}
+
+    public String getTitle() {return title;}
+
+    public void setTitle(String title) {this.title = title;}
+
+    public String getDescription() {return description;}
+
+    public void setDescription(String description) {this.description = description;}
+
+    public int getReleaseYear() {return releaseYear;}
+
+    public void setReleaseYear(int releaseYear) {this.releaseYear = releaseYear;}
+
+    public int getRuntimeMinutes() {return runtimeMinutes;}
+
+    public void setRuntimeMinutes(int runtimeMinutes) {this.runtimeMinutes = runtimeMinutes;}
+
+    public String getStorageLocation() {return storageLocation;}
+
+    public void setStorageLocation(String storageLocation) {this.storageLocation = storageLocation;}
+}
