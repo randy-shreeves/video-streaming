7fce82197c23c8defc480bfbeb4fa8d3fa9eeabd
randy <shreevesrandy@gmail.com>
2026-06-24 21:21:29 -0400

    Create MovieService and add methods createMovie() and getAllMovies()

---
+++ src/main/java/com/randyshreeves/videostreaming/movie/MovieService.java
@@ -0,0 +1,23 @@
+package com.randyshreeves.videostreaming.movie;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+
+    private final MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public void createMovie(Movie movie) {
+        movieRepository.save(movie);
+    }
+
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+}
