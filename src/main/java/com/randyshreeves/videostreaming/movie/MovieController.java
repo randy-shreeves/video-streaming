3e9892d380af513cf0be86761ea2d4864d3f8983
randy <shreevesrandy@gmail.com>
2026-06-25 10:04:08 -0400

    Create MovieController

---
+++ src/main/java/com/randyshreeves/videostreaming/movie/MovieController.java
@@ -0,0 +1,27 @@
+package com.randyshreeves.videostreaming.movie;
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping
+    public List<Movie> getAllMovies() {
+        return movieService.getAllMovies();
+    }
+
+    @PostMapping
+    public void createMovie(@RequestBody Movie movie) {
+        movieService.createMovie(movie);
+    }
+}
