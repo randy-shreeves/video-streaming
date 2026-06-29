cc2d23adc48f85aaba4b5b212d14b876541f3e34
randy <shreevesrandy@gmail.com>
2026-06-28 21:18:58 -0400

    Re-name application class file to match new package structure

--- backend/src/main/java/com/randyshreeves/videostreaming/VideostreamingApplication.java
+++ backend/src/main/java/com/randyshreeves/videostreaming/VideostreamingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VideostreamingApplication {
+public class VideoStreamingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VideostreamingApplication.class, args);
+		SpringApplication.run(VideoStreamingApplication.class, args);
 	}
 
 }
