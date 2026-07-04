c13e1c8ade3e318634826eee9d7a34b1321be0ed
randy <shreevesrandy@gmail.com>
2026-07-03 22:20:52 -0400

    Created MovieListPage and transferred movie list fetching logic from App to MovieListPage.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,7 @@
-import { useEffect, useState } from 'react';
-import { getMovies } from "./api/movieApi";
-import type { Movie } from "./types/Movie";
+import MovieListPage from "./pages/MovieListPage";
 
 function App() {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  useEffect(() => {
-    async function loadMovies() {
-      try {
-        const movieList = await getMovies();
-        setMovies(movieList);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    loadMovies();
-  }, []);
-
-  return (
-    <>
-      <h1>Video Streaming</h1>
-
-      {movies.map(movie => (
-        <p key={movie.id}>
-          {movie.title}
-        </p>
-      ))}
-    </>
-  );
+  return <MovieListPage />;
 }
 
 export default App;
---
+++ frontend/src/pages/MovieListPage.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import { getMovies } from "../api/movieApi";
+import type { Movie } from "../types/Movie";
+
+function MovieListPage() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  useEffect(() => {
+    async function loadMovies() {
+      try {
+        const movieList = await getMovies();
+        setMovies(movieList);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    loadMovies();
+  }, []);
+
+  return (
+    <>
+      <h1>Video Streaming</h1>
+
+      {movies.map(movie => (
+        <p key={movie.id}>
+          {movie.title}
+        </p>
+      ))}
+    </>
+  );
+}
+
+export default MovieListPage;
