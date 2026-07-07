1e5324513c36d8889e3195b860c0ddba982c79cc
randy <shreevesrandy@gmail.com>
2026-07-07 13:09:21 -0400

    Add video player to WatchPage and access to backend movies/{id}/stream endpoint.

--- frontend/src/pages/WatchPage.tsx
+++ frontend/src/pages/WatchPage.tsx
@@ -1,6 +1,19 @@
+import { useParams } from "react-router-dom";
+
 function WatchPage() {
+    const { id } = useParams();
+    const videoUrl = `http://localhost:8080/movies/${id}/stream`;
     return (
-        <h1>Watch Movie</h1>
+        <>
+            <video controls style={{
+                width: "100%",
+                maxWidth: "900px",
+                height: "500px",
+                objectFit: "contain"
+            }}>
+                <source src={videoUrl} type="video/mp4" />
+            </video>
+        </>
     );
 }
 
