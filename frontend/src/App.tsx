10e367104fcc1f77014b670f8110a4b163d37805
randy <shreevesrandy@gmail.com>
2026-07-26 15:13:16 -0400

    Add route protection to all /movies* endpoints.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,40 @@ import LoginPage from "./pages/LoginPage";
 import MovieListPage from "./pages/MovieListPage";
 import MovieDetailPage from "./pages/MovieDetailPage";
 import WatchPage from "./pages/WatchPage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/movies" element={<MovieListPage />} />
-      <Route path="/movies/:id" element={<MovieDetailPage />} />
-      <Route path="/movies/:id/watch" element={<WatchPage />} />
+
+      <Route path="/" element={<LoginPage />}/>
+
+      <Route 
+        path="/movies" 
+        element={
+          <ProtectedRoute>
+            <MovieListPage />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route 
+        path="/movies/:id" 
+        element={
+          <ProtectedRoute>
+            <MovieDetailPage />
+          </ProtectedRoute>
+        } 
+      />
+
+      <Route 
+        path="/movies/:id/watch" 
+          element={
+            <ProtectedRoute>
+              <WatchPage />
+            </ProtectedRoute>
+          } 
+        />
     </Routes>
   );
 }
