0cae77990446666981bd0a2d877343c3310d03be
randy <shreevesrandy@gmail.com>
2026-07-27 16:42:47 -0400

    Update LoginRequest and NewUserRegistrationRequest validations.

--- backend/src/main/java/com/randyshreeves/videostreaming/auth/dto/LoginRequest.java
+++ backend/src/main/java/com/randyshreeves/videostreaming/auth/dto/LoginRequest.java
@@ -1,8 +1,16 @@
 package com.randyshreeves.videostreaming.auth.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class LoginRequest {
 
+    @NotBlank(message = "Username is required.")
+    @Size(min = 3, max = 30, message = "Username must be 3-30 characters.")
     private String username;
+
+    @NotBlank(message = "Password is required.")
+    @Size(min = 8, max = 30, message = "Password must be 8-30 characters.")
     private String password;
 
     public LoginRequest() {}
--- backend/src/main/java/com/randyshreeves/videostreaming/auth/dto/NewUserRegistrationRequest.java
+++ backend/src/main/java/com/randyshreeves/videostreaming/auth/dto/NewUserRegistrationRequest.java
@@ -6,11 +6,11 @@ import jakarta.validation.constraints.Size;
 public class NewUserRegistrationRequest {
 
     @NotBlank(message = "Username is required.")
-    @Size(min = 3, max = 30, message = "Username cannot be greater than 30 characters.")
+    @Size(min = 3, max = 30, message = "Username must be 3-30 characters.")
     private String username;
 
     @NotBlank(message = "Password is required.")
-    @Size(min = 8, max = 30, message = "Password cannot be greater than 30 characters.")
+    @Size(min = 8, max = 30, message = "Password must be 8-30 characters.")
     private String password;
 
     public NewUserRegistrationRequest() {}
