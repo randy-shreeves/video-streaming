308a3ae65506134134ae5c64733ab651af04b0f5
randy <shreevesrandy@gmail.com>
2026-08-31 15:17:23 -0400

    Update WatchlistRepository to return Page rather than List.

--- backend/src/main/java/com/randyshreeves/videostreaming/watchlist/WatchlistRepository.java
+++ backend/src/main/java/com/randyshreeves/videostreaming/watchlist/WatchlistRepository.java
@@ -1,13 +1,14 @@
 package com.randyshreeves.videostreaming.watchlist;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface WatchlistRepository extends JpaRepository<Watchlist, Long> {
 
-    List<Watchlist> findByUserIdOrderByIdAsc(Long userId);
+    Page<Watchlist> findByUserIdAndMoviePublishedTrueOrderByIdAsc(Long userId, Pageable pageable);
     boolean existsByUserIdAndMovieId(Long userId, Long movieId);
     Optional<Watchlist> findByUserIdAndMovieId(Long userId, Long movieId);
 }
